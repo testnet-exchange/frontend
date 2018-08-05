c727aff65f77e10bd0e24c6c0ce4c49f9d68f8fd
Aleksey Bykhun <theisease@gmail.com>
2018-08-06 00:55:26 +0300

    remove warnings

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,15 +6,15 @@ import Order from '../../components/Order/Order'
 import NewUser from './NewUser/NewUser'
 import Registered from './Registered/Registered'
 
-
-const titles = ['Cm. Vol', 'Cm. Cost', 'Cost', 'Volume', 'Price']
-
-const rows = [
-  { cmvol: '0.123', cmcost: '1.124',  cost: '12',  volume: '12344', price: '100' },
-  { cmvol: '0.12',  cmcost: '2.123',  cost: '12',  volume: '12344', price: '100' },
-  { cmvol: '0.23',  cmcost: '1.1123', cost: '12',  volume: '12344', price: '100' },
-  { cmvol: '0.3',   cmcost: '1.13',   cost: '12',  volume: '12344', price: '100' },
-]
+//
+// const titles = ['Cm. Vol', 'Cm. Cost', 'Cost', 'Volume', 'Price']
+//
+// const rows = [
+//   { cmvol: '0.123', cmcost: '1.124',  cost: '12',  volume: '12344', price: '100' },
+//   { cmvol: '0.12',  cmcost: '2.123',  cost: '12',  volume: '12344', price: '100' },
+//   { cmvol: '0.23',  cmcost: '1.1123', cost: '12',  volume: '12344', price: '100' },
+//   { cmvol: '0.3',   cmcost: '1.13',   cost: '12',  volume: '12344', price: '100' },
+// ]
 
 class Home extends Component {
 
