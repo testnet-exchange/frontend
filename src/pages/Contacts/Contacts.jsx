43b8e247a2a1893e0081e570b0fa2093a9c9c7c9
Daniil Okhlopkov <ohl.d@yandex.ru>
2018-08-12 03:40:43 +0300

    Update Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Contacts = () => (
   <section>
     <h1>Contacts</h1>
+    
+    <a href="https://github.com/testnet-exchange">GitHub</a>
+    <br></br>
+    <a href="https://t.me/testnet_exchange">Telegram Chat</a>
 
   </section>
 )
